56b53894dd1e87ae356f7042e5e43c536d26b759
anthatipraveen821-creator <anthatipraveen821@gmail.com>
2026-03-03 14:23:14 -0600

    Add project files.

---
+++ DapperDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DapperDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DapperDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        // EF Core DbSet for Company
+        public DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ DapperDemo/Models/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace DapperDemo.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public int Name { get; set; }
+        public int Address { get; set; }
+        public int City { get; set; }
+        public int State { get; set; }
+        public int PostalCode { get; set; }
+
+    }
+}
